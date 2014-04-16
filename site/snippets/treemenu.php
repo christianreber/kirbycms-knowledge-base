ef3a3f64f05355ec29cbd981ab6e179a46e8d572
Lukas Bestle <lukas@lu-x.me>
2014-04-16 20:27:02 +0200

    Simplify treemenu.php
    This change does exact the same thing as the code block above, but is quite a bit shorter and easier to understand. :)

--- site/snippets/treemenu.php
+++ site/snippets/treemenu.php
@@ -10,31 +10,7 @@
 	<a<?= ($p->isActive()) ? ' class="active"' : '' ?> href="<?= $p->url() ?>"><?= $p->title() ?></a>
 			
 			<?php
-				$request_URI = $_SERVER['REQUEST_URI'];
-				
-				// Is this the a subpage?
-				if (strlen($request_URI) > 1)
-				{
-					$url = explode('/', substr($request_URI, 1));
-					
-					for ($i = 0; $i < count($url); $i++)
-						$url_array[] = implode("/", array_slice($url, 0, $i+1));
-
-					// Generate the uri of the menu link that will be generated (e. g. /how-we-work)
-					$page_url = parse_url($p->url());
-					$page_url = $page_url['path'];
-					$page_url = substr($page_url, 1);
-				}
-
-				// Generate submenu for max. 1 level
-				if ($p->depth()<2)
-				{
-					snippet('treemenu', array('subpages' => $p->children()));
-				}
-
-				// Generate submenu ONLY if you selected the parent menu
-				else if (!empty($page_url) AND !empty($url_array) AND in_array($page_url, $url_array))
-				{
+				if(($p->depth() < 2 && $p->hasChildren()) || $page == $p || $page->isDescendantOf($p)) {
 					snippet('treemenu', array('subpages' => $p->children()));
 				}
 			?>
