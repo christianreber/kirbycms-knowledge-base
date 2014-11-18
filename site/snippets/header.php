e3475ecce229255547c1b44027c4baddba74438a
verkruemelt <verkruemelt@gmail.com>
2014-11-18 07:35:30 +0100

    reflect new structure in header snippet
    Signed-off-by: verkruemelt <verkruemelt@gmail.com>

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	
+
 	<title><?= html($page->title()) ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
-	<?= css('assets/styles/styles.css') ?>
-	<?= css('assets/styles/php.css') ?>
-	<?= css('assets/styles/html.css') ?>
-	<?= css('assets/styles/js.css') ?>
-	<?= css('assets/styles/css.css') ?>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<?= css('assets/css/main.css') ?>
+	<?= css('assets/css/php.css') ?>
+	<?= css('assets/css/html.css') ?>
+	<?= css('assets/css/js.css') ?>
+	<?= css('assets/css/css.css') ?>
 </head>
 
 <body>
-	
+
 	<div id="wrapper">
-		
+
 		<div class="menu">
 			<a id="logo" href="./"></a>
 			<form id="search" action="./search"><input type="search" name="q" placeholder="Search..."></form>
-			<?php snippet('treemenu') ?>			
+			<?php snippet('treemenu') ?>
 		</div>
